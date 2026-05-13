e98c52cd6818ae9ab92dc40e1d78ced10ff6146a
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-13 18:15:39 -0300

    feat: implementação modo JWT e testes

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,8 +2,14 @@ from rest_framework import generics
 from apps.users.models import User
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.generics import RetrieveAPIView
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]  
+    permission_classes = [AllowAny] 
     
+class ProfileView(RetrieveAPIView):
+    def get_object(self):
+        return self.request.user
+    serializer_class = UserSerializer
